7759d9e6d557071b81456995f551827e784b9fd5
Alexander7145 <aberofeevy@gmail.com>
2024-01-31 14:12:40 +0300

    Домашка часть 1.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,90 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+
+
+
+        byte potato = 100;
+        System.out.println(" значение переменной potato с типом byte равно " +potato);
+        short eggs = 1000;
+        System.out.println(" значение переменной eggs с типом short равно " +eggs);
+        int onion = 10000;
+        System.out.println("значение переменной onion с типом int равно " +onion);
+        long tomato = 100000;
+        System.out.println("значение переменной tomato c типом long равно " +tomato);
+        float sugar = 3.75f;
+        System.out.println("значение переменной sugar c типом float равно " +sugar);
+        double coarseMeal = 5.757;
+        System.out.print("значение переменной coarseMeal с типом double равно "  +coarseMeal);
+
+
+        float a = 27.12f;
+        System.out.println(a);
+        long b = 987678965549L;
+        System.out.println(b);
+        double c = 2.786;
+        System.out.print(c);
+        short d = 569;
+        System.out.print(d);
+        short e = -159;
+        System.out.print(e);
+        short f = 27897;
+        System.out.print(f);
+        byte g = 67;
+        System.out.print(g);
+
+        byte ludmilaPavlovna = 23;
+        byte annaSergeevna = 27;
+        byte ekaterinaAndreevna = 30;
+        int totalUnit = ludmilaPavlovna + annaSergeevna + ekaterinaAndreevna;
+        int paperUnit = 480 / totalUnit;
+        System.out.print(" на каждого ученика рассчитано " +paperUnit+ " листов бумаги");
+
+
+        byte bottleMaker = 16;
+        short twentyMinutes = 20 / 2 * 16;
+        System.out.println(" за 20 минут машина произвела " + twentyMinutes+ " бутылок ");
+        short oneDay = 24 * 60 / 2;
+        System.out.println(" за сутки машина произвела " + oneDay+ " бутылок");
+        int threeDays = oneDay * 3;
+        System.out.println(" за 3 дня машина произвела " + threeDays+ " бутылок");
+        int oneMonth = oneDay * 30;
+        System.out.println(" за один месяц машина произвела " + oneMonth+ " бутылок");
+
+
+        byte classQuantity = 120 / (2 + 4);
+        int whitePaint = classQuantity * 2;
+        int brownPaint = classQuantity * 4;
+        System.out.println("в школе, где " + classQuantity + " классов, нужно " + whitePaint + " банок белой краски и " + brownPaint + " банок коричневой краски");
+
+
+        short bananaPart = 80 * 5;
+        short milkPart = 105 * 2;
+        short iceCream = 100 * 2;
+        short eggPart = 70 * 4;
+        int totalWeight = bananaPart + milkPart + iceCream + eggPart;
+        float totalKg = totalWeight / 1000f;
+        System.out.println(" вес спортзавтрака составляет " + totalWeight+ " грамм");
+        System.out.println(" вес спортзавтрака составляет " + totalKg+ " килограмм");
+
+
+
+        short longWay = 7000 / 250;
+        short shortWay = 7000 / 500;
+        System.out.println(" при простой диете потребуется на похудение " +longWay+ " дней");
+        System.out.println(" при жесткой диете потребуется на похудение " +shortWay+ " дней");
+
+        int mashaSalary = 67760;
+        int denisSalary = 83690;
+        int kristinaSalary = 76230;
+        float mashaNew = mashaSalary + (mashaSalary * 0.1f);
+        float denisNew = denisSalary + (denisSalary * 0.1f);
+        float kristinaNew = kristinaSalary + (kristinaSalary * 0.1f);
+        float mashaYear = (mashaSalary * 0.1f) * 12;
+        float denisYear = (denisSalary * 0.1f) * 12;
+        float kristinaYear = (kristinaSalary * 0.1f) * 12;
+        float totalYear = mashaYear + denisYear + kristinaYear;
+        System.out.println(" маша теперь получает " + mashaNew+ " рублей. Годовой доход вырос на " + mashaYear+ " рублей");
+        System.out.println(" денис теперь получает " + denisNew+ " рублей. Годовой доход вырос на " + denisYear+ " рублей");
+        System.out.println(" кристина теперь получает " + kristinaNew+ " рублей. Годовой доход вырос на " +kristinaYear+ " рублей");
+        System.out.println(" общая разница в годовом доходе составляет " + totalYear + " рублей");
+}}
